de066892095542c8c15828104004b986deebef94
Olivia Zhang <oliviazhang@MacBook-Pro.local>
2017-10-03 16:34:41 -0400

    initialize repository

---
+++ db/migrate/20171003191859_create_groups.rb
@@ -0,0 +1,12 @@
+class CreateGroups < ActiveRecord::Migration
+  def change
+    create_table :groups do |t|
+      t.string :name,
+      t.string :time,
+      t.string :location,
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
